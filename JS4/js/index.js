3b76df3a821885227d60e25f7b57facc36b2c44b
VyacheslavPetrov <slava-sort@rambler.ru>
2020-09-07 11:03:18 +0300

    JS4 sort by

--- JS4/js/index.js
+++ JS4/js/index.js
@@ -34,11 +34,11 @@ basicData(arr)
 * TODO переименуй в осмысленные константы (const, вместо let), к примеру вместо firstField -> fieldName, а
 *    вместо secondField -> fieldDescription
 ***/
-let addBtn = document.querySelector('.btn');
-let firstField = document.querySelector('input:nth-of-type(1)');
-let secondField = document.querySelector('input:nth-of-type(2)');
-let thirdField = document.querySelector('input:nth-of-type(3)');
-let fourthField = document.querySelector('input:nth-of-type(4)');
+const addBtn = document.querySelector('.btn');
+const fieldName = document.querySelector('input:nth-of-type(1)');
+const fieldDescription = document.querySelector('input:nth-of-type(2)');
+const fieldInfo1 = document.querySelector('input:nth-of-type(3)');
+const fieldInfo2 = document.querySelector('input:nth-of-type(4)');
 
 
 const fields = document.querySelectorAll('input[type=text]')
@@ -48,7 +48,7 @@ const fields = document.querySelectorAll('input[type=text]')
 
 const addData = e => {
 	e.preventDefault();
-	if(!firstField.value || !secondField.value || !thirdField.value || !fourthField.value){
+	if(!fieldName.value || !fieldDescription.value || !fieldInfo1.value || !fieldInfo2.value){
 	    alert("Заполните все поля!")
 	    return false
     }
@@ -58,16 +58,42 @@ const addData = e => {
     for(let i = 0; i < fields.length; i++){
         newObj[fields[i].name] = fields[i].value
     }
-    basicData([newObj]);
+    arr.push(newObj);
+    basicData(arr.slice(-1));
 
     for(let i = 0; i < fields.length; i++){
         fields[i].value = '';
     }
 }
-
 addBtn.addEventListener("click", addData)
 
-
+//Сортировка
+const sortId = document.querySelector('.id');
+const sortName = document.querySelector('.name');
+const sortDescription = document.querySelector('.description');
+const sortInfo1 = document.querySelector('.info1');
+const sortInfo2 = document.querySelector('.info2');
+const sortById = () => {
+	arr.sort((a, b) => a.id > b.id ? 1 : -1);
+}
+const sortByName = () => {
+	arr.sort((a, b) => a.name > b.name ? 1 : -1);
+	
+}
+const sortByDescription = () => {
+	arr.sort((a, b) => a.description > b.description ? 1 : -1);
+}
+const sortByInfo1 = () => {
+	arr.sort((a, b) => a.info1 > b.info1 ? 1 : -1);
+}
+const sortByInfo2 = () => {
+	arr.sort((a, b) => a.info2 > b.info2 ? 1 : -1);
+}
+sortId.addEventListener("click", sortById);
+sortName.addEventListener("click", sortByName);
+sortDescription.addEventListener("click", sortByDescription);
+sortInfo1.addEventListener("click", sortByInfo1);
+sortInfo2.addEventListener("click", sortByInfo2);
 
 //time 4ч
 
