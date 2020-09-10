b6d7d23e6246d7f7471f45e4e9bea9949b416fb2
VyacheslavPetrov <slava-sort@rambler.ru>
2020-09-10 08:11:15 +0300

    JS4 sort by

--- JS4/js/index.js
+++ JS4/js/index.js
@@ -5,6 +5,20 @@ const arr = [
 	{id: 4, name: "Vova", description: "description", info1: "info", info2: "info"}
 ]
 
+//Переменные
+const addBtn = document.querySelector('.btn');
+const fieldName = document.querySelector('input:nth-of-type(1)');
+const fieldDescription = document.querySelector('input:nth-of-type(2)');
+const fieldInfo1 = document.querySelector('input:nth-of-type(3)');
+const fieldInfo2 = document.querySelector('input:nth-of-type(4)');
+const fields = document.querySelectorAll('input[type=text]')
+const sortId = document.querySelector('.id');
+const sortName = document.querySelector('.name');
+const sortDescription = document.querySelector('.description');
+const sortInfo1 = document.querySelector('.info1');
+const sortInfo2 = document.querySelector('.info2');
+
+//Добавление Чекбокса
 const addCheckbox = newRow => {
     const newTDBtn = document.createElement('td');
     newRow.append(newTDBtn);
@@ -13,6 +27,7 @@ const addCheckbox = newRow => {
     newTDBtn.append(newBtn);
 }
 
+//Заполение таблицы из массива
 const basicData = ars => {
 	const newTbody = document.querySelector('tbody')
     const TRS = newTbody.querySelectorAll("tr")
@@ -35,14 +50,8 @@ const basicData = ars => {
 
 basicData(arr)
 
-const addBtn = document.querySelector('.btn');
-const fieldName = document.querySelector('input:nth-of-type(1)');
-const fieldDescription = document.querySelector('input:nth-of-type(2)');
-const fieldInfo1 = document.querySelector('input:nth-of-type(3)');
-const fieldInfo2 = document.querySelector('input:nth-of-type(4)');
 
-const fields = document.querySelectorAll('input[type=text]')
-
+//Добавление новой строки в таблицу
 const addData = e => {
 	e.preventDefault();
 	if(!fieldName.value || !fieldDescription.value || !fieldInfo1.value || !fieldInfo2.value){
@@ -62,91 +71,38 @@ const addData = e => {
         fields[i].value = '';
     }
 }
-addBtn.addEventListener("click", addData)
+
 
 //Сортировка
-const sortId = document.querySelector('.id');
-const sortName = document.querySelector('.name');
-const sortDescription = document.querySelector('.description');
-const sortInfo1 = document.querySelector('.info1');
-const sortInfo2 = document.querySelector('.info2');
 
 const sortIncrease = (field, fieldName) => {
 	arr.sort((a, b) => a[fieldName] > b[fieldName] ? 1 : -1);
     basicData(arr);
-    sortId.querySelector(".increase").style.display = "none"
-    sortId.querySelector(".degrease").style.display = "inline"
+    field.querySelector(".increase").style.display = "none"
+    field.querySelector(".degrease").style.display = "inline"
 }
 
-const sortDegrease = (field) => {
-    arr.sort((a, b) => a.id > b.id ? -1 : 1);
+const sortDegrease = (field, fieldName) => {
+    arr.sort((a, b) => a[fieldName] > b[fieldName] ? -1 : 1);
     basicData(arr);
-    sortId.querySelector(".increase").style.display = "inline"
-    sortId.querySelector(".degrease").style.display = "none"
+    field.querySelector(".increase").style.display = "inline"
+    field.querySelector(".degrease").style.display = "none"
 }
 
-const sortByName = () => {
-	arr.sort((a, b) => a.name > b.name ? 1 : -1);
-	basicData(arr);
-}
-const sortByDescription = () => {
-	arr.sort((a, b) => a.description > b.description ? 1 : -1);
-	basicData(arr);
-}
-const sortByInfo1 = () => {
-	arr.sort((a, b) => a.info1 > b.info1 ? 1 : -1);
-	basicData(arr);
-}
-const sortByInfo2 = () => {
-	arr.sort((a, b) => a.info2 > b.info2 ? 1 : -1);
-	basicData(arr);
-}
-
-// TODO Объедени все методы в метод sortTable(row)
-
+//Событие добавление строки 
+addBtn.addEventListener("click", addData)
+//События сортировки
 sortId.querySelector(".increase").addEventListener("click", () => sortIncrease(sortId, 'id'));
 sortId.querySelector(".degrease").addEventListener("click", () => sortDegrease(sortId, 'id'));
+sortName.querySelector(".increase").addEventListener("click", () => sortIncrease(sortName, 'name'));
+sortName.querySelector(".degrease").addEventListener("click", () => sortDegrease(sortName, 'name'));
+sortDescription.querySelector(".increase").addEventListener("click", () => sortIncrease(sortDescription, 'description'));
+sortDescription.querySelector(".degrease").addEventListener("click", () => sortDegrease(sortDescription, 'description'));
+sortInfo1.querySelector(".increase").addEventListener("click", () => sortIncrease(sortInfo1, 'info1'));
+sortInfo1.querySelector(".degrease").addEventListener("click", () => sortDegrease(sortInfo1, 'info1'));
+sortInfo2.querySelector(".increase").addEventListener("click", () => sortIncrease(sortInfo2, 'info2'));
+sortInfo2.querySelector(".degrease").addEventListener("click", () => sortDegrease(sortInfo2, 'info2'));
 
-sortName.addEventListener("click", () => sortByName());
-sortDescription.addEventListener("click", sortByDescription);
-sortInfo1.addEventListener("click", sortByInfo1);
-sortInfo2.addEventListener("click", sortByInfo2);
+
 
 //time 4ч
-
-
-
-/* На всякий случай
-
-let BasicData = (ars) => {
-	for(i = 0; i < ars.length; i++) {
-		let newRow = document.createElement('tr');
-		let newTableRow = tableBoard.append(newRow);
-		
-		let newTDId = document.createElement('td');
-		let newRowTDId = newRow.append(newTDId);
-		newTDId.textContent = arr[i]["id"];
-		let newTDName = document.createElement('td');
-		let newRowTDName = newRow.append(newTDName);
-		newTDName.textContent = arr[i]["name"];
-		let newTDDesc = document.createElement('td');
-		let newRowTDDesc = newRow.append(newTDDesc);
-		newTDDesc.textContent = arr[i]["description"];
-		let newTDInf1 = document.createElement('td');
-		let newRowTDInf1 = newRow.append(newTDInf1);
-		newTDInf1.textContent = arr[i]["info1"];
-		let newTDInf2 = document.createElement('td');
-		let newRowTDInf2 = newRow.append(newTDInf2);
-		newTDInf2.textContent = arr[i]["info2"];
-		let newTDBtn = document.createElement('td');
-		let newRowTDBtn = newRow.append(newTDBtn);
-		let newBtn = document.createElement('input');
-		newBtn.type = "checkbox";
-		let addBtnInTD = newTDBtn.append(newBtn);
-		
-
-	}
-}
-
-BasicData(arr)
-*/
