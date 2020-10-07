bd9d89e5d254360e9caa57bf0a41d2cde3b8ca52
greyewi1 <greyewi@gmail.com>
2020-10-07 13:06:09 +0300

    fix sorted error

--- my-table/my-table/src/App.jsx
+++ my-table/my-table/src/App.jsx
@@ -11,34 +11,26 @@ const tableMockUp = [
   {"id": 4, "name": "Vova", "description": "description", "info1": "info", "info2": "info", "isChecked" : false},
   {"id": 5, "name": "Vasya", "description": "description", "info1": "info", "info2": "info", "isChecked" : false}
 ]
-const useForceUpdate = () => useState()[1];
-const compare = (prev, next) => {
-  console.log(prev)
-  console.log(next)
-}
+
 function App() {
   const [tableData, setTableData] = useState(tableMockUp)
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [info1, setInfo1] = useState('')
   const [info2, setInfo2] = useState('')
-  const forceUpdate = useForceUpdate();
 
-  useEffect(() => {
-    forceUpdate()
-  }, [compare(tableData)])
   const handleAddData = useCallback(() => {
     setTableData(tableData.concat([{id: tableData.length+1, name: name, description: description, info1: info1, info2: info2, isChecked: false}]))
-
   }, [tableData.length, name, description, info1, info2])
+
   const handleSortById = () => {
-    console.log(tableData.sort((a, b) => a.id > b.id ? -1 : 1))
-    setTableData(tableData.sort((a, b) => a.id > b.id ? -1 : 1))
-    forceUpdate()
+    const sorted = [...tableData].sort((a, b) => b.id - a.id);
+    setTableData(sorted)
   }
+
   const fields = {name, description, info1, info2}
   const handleEvents = {setName, setDescription, setInfo1, setInfo2, handleAddData}
-  console.log(tableData)
+
   return (
     <div id="app">
       <header>
@@ -49,7 +41,6 @@ function App() {
         <Table data={tableData} handleSortById={handleSortById}/>
         <Edit />
       </main>
-
     </div>
   );
 }
